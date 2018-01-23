4896665fb70c738b6d45f8d0daf5a2a5a9e97f20
Andres Ramos <andresmao2@gmail.com>
2018-01-23 10:59:43 -0500

    Add bootstrap css and js to support themes without bootstrap

--- view.php
+++ view.php
@@ -29,7 +29,16 @@ require_once('locallib.php');
 
 global $DB, $CFG, $PAGE;
 
-$CFG->additionalhtmlhead .= '<style>
+$CFG->additionalhtmlhead .= '
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<style>
     .card {
         border: 0px !important;
         background-color: transparent;
